50c30041bb05045a2df28d23ebcb468f3957413e
KayleeWilliams <65376239+KayleeWilliams@users.noreply.github.com>
2023-03-02 14:32:44 +0000

    feat: :sparkles: Created Booking Model

--- Project/UWEFlix/models.py
+++ Project/UWEFlix/models.py
@@ -20,3 +20,13 @@ class Ticket(models.Model):
 class TicketTypeQuantity(models.Model):
         ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
         quantity = models.PositiveIntegerField()
+
+# Booking Model
+class Booking(models.Model):
+    showing = models.ForeignKey(Showing, on_delete=models.CASCADE)
+    customer_name = models.CharField(max_length=100)
+    customer_email = models.EmailField()
+    total_cost = models.DecimalField(max_digits=8, decimal_places=2)
+
+    # User can select multiple ticket types and quantities
+    ticket_type_quantities = models.ManyToManyField(TicketTypeQuantity)
