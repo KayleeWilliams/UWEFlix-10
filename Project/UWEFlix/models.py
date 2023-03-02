0a59194a0a0173fdd0a7050cf32dc70201c2c109
KayleeWilliams <65376239+KayleeWilliams@users.noreply.github.com>
2023-03-02 14:26:42 +0000

    feat: :sparkles: Ticket Model

--- Project/UWEFlix/models.py
+++ Project/UWEFlix/models.py
@@ -10,3 +10,13 @@ class Showing(models.Model):
         time = models.TimeField()
         tickets_sold = models.IntegerField()
         screen = models.ForeignKey(Screen, on_delete=models.CASCADE)
+
+# Available tickets
+class Ticket(models.Model):
+        name = models.CharField(max_length=100)
+        price = models.DecimalField(max_digits=6, decimal_places=2)
+
+# Ticket type & quantity
+class TicketTypeQuantity(models.Model):
+        ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+        quantity = models.PositiveIntegerField()
