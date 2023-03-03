06c78106b72ca52afd5e81ae10e60b4edfcf59fc
KayleeWilliams <65376239+KayleeWilliams@users.noreply.github.com>
2023-03-03 23:40:25 +0000

    feat: :sparkles: Created frontend for the login system

---
+++ Project/UWEFlix/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField()
+
--- Project/UWEFlix/views.py
+++ Project/UWEFlix/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import LoginForm
 
 # Create your views here.
+
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            
+            # Dummy error to test error handling
+            if email == password:  
+              form.add_error(None, "Email and password cannot be the same.")
+              return render(request, 'login.html', {'form': form, 'errors': form.errors})
+            else:
+              return HttpResponse("Logged In")
+        else:
+            form.add_error(None, "An unknown error occurred")
+            return render(request, 'login.html', {'form': form, 'errors': form.errors})
+    else:
+      form = LoginForm()
+      return render(request, 'login.html', {'form': form})
+
+
+def index(request):
+    return HttpResponse("Index")
