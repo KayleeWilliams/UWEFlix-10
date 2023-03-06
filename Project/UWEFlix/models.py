78180b6356f7fe00af48b1f7ded5fa357d135551
KayleeWilliams <65376239+KayleeWilliams@users.noreply.github.com>
2023-03-06 17:56:33 +0000

    feat: :sparkles: Added seperate user model to the booking model

--- Project/UWEFlix/models.py
+++ Project/UWEFlix/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Screen(models.Model):
@@ -11,22 +12,24 @@ class Showing(models.Model):
         tickets_sold = models.IntegerField()
         screen = models.ForeignKey(Screen, on_delete=models.CASCADE)
 
+
 # Available tickets
 class Ticket(models.Model):
         name = models.CharField(max_length=100)
         price = models.DecimalField(max_digits=6, decimal_places=2)
 
 # Ticket type & quantity
+
 class TicketTypeQuantity(models.Model):
         ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
         quantity = models.PositiveIntegerField()
 
 # Booking Model
-class Booking(models.Model):
-    showing = models.ForeignKey(Showing, on_delete=models.CASCADE)
-    customer_name = models.CharField(max_length=100)
-    customer_email = models.EmailField()
-    total_cost = models.DecimalField(max_digits=8, decimal_places=2)
 
-    # User can select multiple ticket types and quantities
-    ticket_type_quantities = models.ManyToManyField(TicketTypeQuantity)
+class Booking(models.Model):
+        showing = models.ForeignKey(Showing, on_delete=models.CASCADE)
+        user = models.ForeignKey(User, on_delete=models.CASCADE)
+        total_cost = models.DecimalField(max_digits=8, decimal_places=2)
+
+        # User can select multiple ticket types and quantities
+        ticket_type_quantities = models.ManyToManyField(TicketTypeQuantity)
