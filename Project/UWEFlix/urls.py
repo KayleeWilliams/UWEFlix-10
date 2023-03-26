7b8af422984ee45041004f44f4bb94b50fc664a1
KayleeWilliams <65376239+KayleeWilliams@users.noreply.github.com>
2023-03-26 20:31:09 +0100

    feat: :sparkles: Added Film Management for Cinema Manager

--- Project/UWEFlix/urls.py
+++ Project/UWEFlix/urls.py
@@ -13,5 +13,9 @@ urlpatterns = [
     path('view_account', views.view_account),
     path('modify_account', views.modify_account),
     path('cinema_management', views.cm_dash, name='cinema_management'),
+    path('film_management', views.film_dash, name='film_management'),
+    path('add_film', views.add_film, name='add_film'),
+    path('modify_film', views.modify_film, name='modify_film'),
+    path('delete_film', views.delete_film, name='delete_film'),
 
 ]
