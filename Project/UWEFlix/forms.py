d6c1a409637d4982154e6f8deb915ced6ce1179e
j2-hore <Joshua2.Hore@live.uwe.ac.uk>
2023-03-27 10:40:57 +0100

    Added cinema manager forms to forms.py

--- Project/UWEFlix/forms.py
+++ Project/UWEFlix/forms.py
@@ -7,3 +7,24 @@ class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField()
 
+class FilmForm(forms.Form):
+    title = forms.CharField()
+    rating = forms.CharField()
+    duration = forms.CharField()
+    trailer_desc = forms.CharField()
+
+class ClubForm(forms.Form):
+    name = forms.CharField()
+    address = forms.CharField()
+    contacts = forms.CharField()
+    representative = forms.CharField()
+
+class ScreenForm(forms.Form):
+    capacity = forms.CharField()
+    seat_number = forms.CharField()
+
+class ShowingForm(forms.Form):
+    title = forms.CharField()
+    date = forms.CharField()
+    time = forms.CharField()
+
